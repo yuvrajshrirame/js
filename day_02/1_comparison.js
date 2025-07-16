1870e9f91bc43db69af6567dfb648b4c295509e1
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2025-07-16 19:19:10 +0530

    Learnt comparisons

---
+++ day_02/1_comparison.js
@@ -0,0 +1,21 @@
+// console.log(1 > 2);
+// console.log(1 < 2);
+// console.log(1 >= 2);
+// console.log(1 <= 2);
+// console.log(1 == 2);
+// console.log(1 != 2);
+
+// console.log("1" > 0);
+// console.log("0" > 1);
+
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null != 0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined != 0);
+
+// '===' checks datatype also
