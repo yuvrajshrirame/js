45d4a9254641b6b6f7ee393ebe25fe960b466880
yuvrajshrirame <mail.yuvrajshrirame@gmail.com>
2025-07-16 19:34:01 +0530

    Learnt datatypes

---
+++ day_02/2_datatypes.js
@@ -0,0 +1,20 @@
+// 1. primitive
+
+// seven types: string, number, boolean, null, undefined, symbol, bigint
+
+// 2. non-primitive (reference types)
+
+// array, objects, functions
+
+arr = [1,2,3,4,"lil bieber"]
+
+obj = {
+    name: justin,
+    age: 30
+}
+
+const vrble = function(){
+    console.log("Hello World");
+}
+
+// note: null is of 'object' type
